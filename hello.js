4b1d02a0694596383c7f83e8a30c81f9621eec7c
cool705 <asdfghjklzxcvbnm20231@gmail.com>
2025-10-03 00:06:54 -0400

    Update hello.js

--- hello.js
+++ hello.js
@@ -1,3 +1,10 @@
-export default function handler(req, res) {
-  res.status(200).json({ message: "Hello from Vercel API!" });
-}
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Hello from Rolimons API Scrapper!");
+});
+
+app.listen(3000, () => {
+  console.log("Server running on port 3000");
+});
