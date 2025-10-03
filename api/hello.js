6a997f38392a7f61ee80538ac34c4f194d7037b7
cool705 <asdfghjklzxcvbnm20231@gmail.com>
2025-10-03 00:44:41 -0400

    Update hello.js

--- api/hello.js
+++ api/hello.js
@@ -1,3 +1,12 @@
 export default function handler(req, res) {
-  res.status(200).json({ message: "Hello from API!" });
+    const response = await fetch('https://api.rolimons.com/items/v2/itemdetails');
+    const data = await response.json();
+  const result = Object.entries(data.items).map(([id, values]) => ({
+  id,
+  value: values[3] 
+}));
+
+console.log(result);
+  
+  res.status(200).json({ message: result });
 }
