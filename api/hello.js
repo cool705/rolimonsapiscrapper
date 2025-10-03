f97ac7cbc1fb7e1617dc61eccf725416d8c44004
cool705 <asdfghjklzxcvbnm20231@gmail.com>
2025-10-03 00:32:36 -0400

    Update hello.js

--- api/hello.js
+++ api/hello.js
@@ -1,10 +1,3 @@
-const express = require("express");
-const app = express();
-
-app.get("/", (req, res) => {
-  res.send("Hello from Rolimons API Scrapper!");
-});
-
-app.listen(3000, () => {
-  console.log("Server running on port 3000");
-});
+export default function handler(req, res) {
+  res.status(200).json({ message: "Hello from API!" });
+}
